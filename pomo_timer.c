fc1b7c07d693acd3d70c45655091c326b085f46e
biizerrah <biizerrah@outlook.com>
2025-02-25 00:23:52 -0300

    <UPDATE>: refactoring the code

--- pomo_timer.c
+++ pomo_timer.c
@@ -30,6 +30,48 @@ int main()
     ssd1306_fill(&ssd, false);
     ssd1306_send_data(&ssd);
 
+void focus(){
+
+      ssd1306_fill(&ssd, false);
+      ssd1306_draw_string(&ssd, "TIME TO FOCUS", 20, 30);
+      ssd1306_send_data(&ssd);
+      gpio_put(LED_RED, true);
+      sleep_ms(25000);
+      gpio_put(LED_RED,false);
+      gpio_put(BUZZER, true);
+      sleep_ms(1000); 
+      gpio_put(BUZZER, false);
+      ssd1306_fill(&ssd, false);
+};
+void short_break(){
+
+      ssd1306_fill(&ssd, false);
+      ssd1306_draw_string(&ssd, "5 MIN. BREAK", 20, 30);
+      ssd1306_draw_string(&ssd, "     II", 15, 48);
+      ssd1306_send_data(&ssd);
+      gpio_put(LED_GREEN, true);
+      sleep_ms(5000);
+      gpio_put(LED_GREEN,false);
+      gpio_put(BUZZER, true);
+      sleep_ms(1000); 
+      gpio_put(BUZZER, false); 
+      ssd1306_fill(&ssd, false);
+};
+void long_break(){
+
+      ssd1306_fill(&ssd, false);
+      ssd1306_draw_string(&ssd, "10 MIN BREAK", 20, 30);
+      ssd1306_draw_string(&ssd, "     II", 15, 48);
+      ssd1306_send_data(&ssd);
+      gpio_put(LED_BLUE, true);
+      sleep_ms(10000);
+      gpio_put(LED_BLUE,false);
+      gpio_put(BUZZER, true);
+      sleep_ms(1000); 
+      gpio_put(BUZZER, false);
+      ssd1306_fill(&ssd, false);
+};
+
     gpio_init(LED_RED);
     gpio_set_dir(LED_RED, GPIO_OUT);
 
@@ -46,85 +88,22 @@ int main()
     gpio_init(BUZZER);
     gpio_set_dir(BUZZER, GPIO_OUT);
 
-    void buzzer_RED(){
-
-        gpio_put(LED_RED, true);
-        sleep_ms(25000);
-        gpio_put(LED_RED,false);
-        gpio_put(BUZZER, true);
-        sleep_ms(1000); 
-        gpio_put(BUZZER, false);
-        
-    };
-
-    void buzzer_GREEN();{
-
-      gpio_put(LED_GREEN, true);
-      sleep_ms(5000);
-      gpio_put(LED_GREEN,false);
-      gpio_put(BUZZER, true);
-      sleep_ms(1000); 
-      gpio_put(BUZZER, false);   
-      
-  };
-  void buzzer_BLUE();{
-
-    gpio_put(LED_BLUE, true);
-    sleep_ms(10000);
-    gpio_put(LED_BLUE,false);
-    gpio_put(BUZZER, true);
-    sleep_ms(1000); 
-    gpio_put(BUZZER, false);
-    
-  };
     while (true) {
       
       ssd1306_fill(&ssd, false);
-      ssd1306_draw_string(&ssd, " PRESS A TO ", 8, 10);
+      ssd1306_draw_string(&ssd, "  PRESS A TO ", 8, 10);
       ssd1306_draw_string(&ssd, "   START", 20, 30);
       ssd1306_send_data(&ssd);
 
       if(!gpio_get(BUTTON_A)){
 
-      ssd1306_fill(&ssd, false);
-      ssd1306_draw_string(&ssd, "TIME TO FOCUS", 20, 30);
-      ssd1306_send_data(&ssd);
-      buzzer_RED();
-      ssd1306_fill(&ssd, false);
+        focus();
+        short_break();
+        focus();
+        short_break();
+        focus();
+        long_break();
       
-      ssd1306_fill(&ssd, false);
-      ssd1306_draw_string(&ssd, "5 MIN. BREAK", 20, 30);
-      ssd1306_send_data(&ssd);
-      buzzer_GREEN();
-      ssd1306_fill(&ssd, false);
-
-      
-      ssd1306_fill(&ssd, false);
-      ssd1306_draw_string(&ssd, "TIME TO FOCUS", 20, 30);
-      ssd1306_send_data(&ssd);
-      buzzer_RED();
-      ssd1306_fill(&ssd, false);
-
-      
-      ssd1306_fill(&ssd, false);
-      ssd1306_draw_string(&ssd, "5 MIN. BREAK", 20, 30);
-      ssd1306_send_data(&ssd);
-      buzzer_GREEN();
-      ssd1306_fill(&ssd, false);
-
-     
-      ssd1306_fill(&ssd, false);
-      ssd1306_draw_string(&ssd, "TIME TO FOCUS", 20, 30);
-      ssd1306_send_data(&ssd);
-      buzzer_RED();
-      ssd1306_fill(&ssd, false);
-
-      ssd1306_fill(&ssd, false);
-      ssd1306_draw_string(&ssd, "10 MIN BREAK", 20, 30);
-      ssd1306_send_data(&ssd);
-      buzzer_BLUE();
-      ssd1306_fill(&ssd, false);
-    
       }
       
       sleep_ms(200);   
